3a53fb6a94825787eb0e90cb4bddf0b7418253ae
egorbarisovi4 <egorbarisovi4@gmail.com>
2019-11-24 20:11:36 +0500

    app - some example functions

--- Lessin_8_examples.js
+++ Lessin_8_examples.js
@@ -16,6 +16,13 @@ let user = {
 }
 // Тип : object , строка : Привет!
 
+let example = {
+    name : 'foo',
+    age : 13
+}
+example.toString = function() {};
+// Тип : object , строка : undefined
+
 let masha = 'Masha';
 
 function showInfo (val) {
@@ -25,4 +32,58 @@ function showInfo (val) {
 
 showInfo (john);
 showInfo (user);
+showInfo (example);
 showInfo (masha);
+
+console.log(user.toString);
+
+function compare (obj1, obj2) {
+    let result = obj1 === obj2 ? 'идентичны' : 'разные';
+    console.log(result);
+}
+
+compare ([].toString, {}.toString); // разные
+compare ([].toString, [].toString); // идентичны
+compare ({}.toString, {}.toString); // идентичны
+
+let user_3 = {name : 'Вася', age : '44'};
+console.log (user_3.hasOwnProperty('age'));
+console.log (user_3.hasOwnProperty('toString'));
+
+function checkProp(obj, propName) {
+    var has = propName in obj ? 'есть' : 'нет';
+    var own = obj.hasOwnProperty(propName) ? 'собственное' : 'прототипа';
+    console.log(`Свойство ${propName} ${has}`);
+
+    if (propName in obj) {
+        console.log(`Свойство ${propName} ${own}`);
+    }
+}
+
+let user_4 = {name : 'Паша', age : '123'};
+
+checkProp(user_4, 'valueOf');
+checkProp(user_4, 'toString');
+checkProp(user_4, 'hasOwnProperty');
+checkProp(user_4, 'name');
+checkProp(user_4, 'age');
+checkProp(user_4, 'phone_number');
+
+const protoUser = {
+    show() {
+        console.log(`Пользователь ${this.getFullName()},
+        возраст ${this.age} лет`);
+    },
+    getFullName() {
+        return `${this.name} ${this.lastName}`
+    }
+};
+
+let customUser = Object.create(protoUser);
+
+customUser.age = 67;
+customUser.name = 'Степашка';
+customUser.lastName = 'Хрюшкин';
+
+customUser.show();
+console.log(customUser.getFullName());
