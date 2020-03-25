1c1f8cb56c725b95d54a7bd57027be523f3f999c
egorbarisovi4 <egorbarisovi4@gmail.com>
2020-03-26 03:32:07 +0500

    app - fix func isValidPositin for lesson 3

--- Lesson_8_task_3.js
+++ Lesson_8_task_3.js
@@ -1,5 +1,9 @@
 // Задание 3
+'use strict';
+
 console.log('Задача 3');
+
+
 let requiredFields = [ 'title', 'price', 'discount' ];
 
 let form1 = {
@@ -11,18 +15,65 @@ let form2 = {
     title: 'Товар Телепорт бытовой VZHIH-101',
     discount: 10
 }
+let form3 = {
+    title: 'Товар Телепорт бытовой VZHIH-101',
+    discount: 10,
+    quantity:15
+}
+let form4 = {
+    title: 'Товар Телепорт бытовой VZHIH-101',
+    discount: 10,
+    quantity:15,
+    price: 7800,
+}
 
-function isValidPosition (item = 0, requiredFields) {
+
+function isValidPosition (form = 0, requiredFields, results = []) {
     requiredFields.forEach(function(requiredField) {
-        if (requiredField in item != true) 
-            throw new Error ('В форме не заполнены необходимые поля');
+        if (requiredField in form) {
+            results[results.length] = true;
+        } else {
+            results[results.length] = false;
+        }
     });
-    return console.log('Форма заполнена верно');
+
+    return results.includes(false) ? false : true;
 }
 
-try {
-    isValidPosition(form2, requiredFields);
+
+
+if (isValidPosition(form1, requiredFields)) 
+{
+    console.log('Форма № 1 заполнена верно');
 }
-catch (e) {
-    console.log('В форме не заполнены необходимые поля');
+else
+{
+    console.log('В форме № 1 не заполнены необходимые поля');
+}
+
+if (isValidPosition(form2, requiredFields)) 
+{
+    console.log('Форма № 2 заполнена верно');
+}
+else
+{
+    console.log('В форме № 2 не заполнены необходимые поля');
+}
+
+if (isValidPosition(form3, requiredFields)) 
+{
+    console.log('Форма № 3 заполнена верно');
+}
+else
+{
+    console.log('В форме № 3 не заполнены необходимые поля');
+}
+
+if (isValidPosition(form4, requiredFields)) 
+{
+    console.log('Форма № 4 заполнена верно');
+}
+else
+{
+    console.log('В форме № 4 не заполнены необходимые поля');
 }
